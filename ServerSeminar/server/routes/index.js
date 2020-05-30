04e9166ca960c21b32d13e263ed76537f9c09371
최정균 <wjdrbs966@naver.com>
2020-05-30 21:44:19 +0900

    오타 수정

--- ServerSeminar/server/routes/index.js
+++ ServerSeminar/server/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/class', require('./class'));
-router.use('product', require('./product'));
+router.use('/product', require('./product'));
 
 module.exports = router;
