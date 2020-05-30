4a4fe2f997826045ae893955643b5a00e380660a
최정균 <wjdrbs966@naver.com>
2020-05-31 01:13:23 +0900

    img 경로 추가

--- ServerSeminar/server/routes/class.js
+++ ServerSeminar/server/routes/class.js
@@ -9,7 +9,7 @@ router.get('/recommand', async(req, res) => {
     const result = {
         "classIdx": "1",
         "name": "나만의 미니멀한 도자 컵, 클래스",
-        "img" : " ",
+        "img" : "https://previews.123rf.com/images/picsfive/picsfive1402/picsfive140200177/26001533-%ED%9D%B0%EC%83%89-%EB%B0%B0%EA%B2%BD%EC%97%90-%ED%9D%B0%EC%83%89-%EC%BB%B5%EC%9D%98-%EB%8B%AB%EC%8A%B5%EB%8B%88%EB%8B%A4.jpg",
         "price": "41600",
         "difficulty": "중",
         "term": "32일",
