280516d901204692c5e5d55ca685eece637f064a
EZYOON <44804022+EZYOON@users.noreply.github.com>
2020-06-02 16:16:35 +0900

    update banner model

---
+++ ServerSeminar/server/models/banner.js
@@ -0,0 +1,21 @@
+const pool = require('../modules/pool');
+const table = 'banner';
+
+const banner = {
+    getBanner : async () =>{
+        query = `SELECT * FROM ${table}`;
+        try {
+            const result = pool.queryParam(query);
+            return result;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('getBanner : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('getBanner : ', err);
+            throw err;
+        }
+    }
+}
+
+module.exports = banner;
