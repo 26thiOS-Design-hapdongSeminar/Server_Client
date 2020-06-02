f9d370c0214b47d65b6b69ec26e0c708585737b3
EZYOON <44804022+EZYOON@users.noreply.github.com>
2020-06-02 16:17:14 +0900

    test

---
+++ ServerSeminar/server/models/class.js
@@ -0,0 +1,35 @@
+const pool = require('../modules/pool');
+const table = 'class';
+
+const classModels = {
+    getRecommendClass : async () => {
+        const query = `SELECT * FROM ${table} AS a JOIN classRecommend AS b ON (a.classIdx = b.classIdx)`;
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('getRecommendClass : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('getRecommendClass : ', err);
+            throw err;
+        }
+    },
+    getProgressClassPopular : async () =>{
+        const query = `SELECT * FROM ${table} AS a JOIN progress AS b ON (a.classIdx = b.classIdx) WHERE status="인기"`
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('getProgressClassPopular : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('getProgressClassPopular : ', err);
+            throw err;
+        }
+    }
+}
+
+module.exports = classModels;
--- ServerSeminar/server/routes/banner.js
+++ ServerSeminar/server/routes/banner.js
@@ -3,39 +3,12 @@ const util = require('../modules/util');
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
 var router = express.Router();
+let Banner = require('../models/banner');
 
 
 router.get('/', async(req, res) => {
-    const result = [
-        {
-            "imgIdx" : "1",
-            "img" : "https://image.idus.com/image/files/b34053a7dbf84a7abdf9d91c0a75910f.jpg"
-        },
-        {
-            "imgIdx" : "2",
-            "img" : "https://image.idus.com/image/files/42a3b4d58579414e9a701ffdc7357947.jpg"
-        },
-        {
-            "imgIdx" : "3",
-            "img" : "https://image.idus.com/image/files/08db9334ca304dd38a2f72822a51ae66.jpg"
-        },
-        {
-            "imgIdx" : "4",
-            "img" : "https://image.idus.com/image/files/70e36376ecdc424c965d664e0bf151b7.jpg"
-        },
-        {
-            "imgIdx" : "5",
-            "img" : "https://image.idus.com/image/files/17b0a44e163a496db152e1679b967c15.png"
-        },
-        {
-            "imgIdx" : "6",
-            "img" : "https://image.idus.com/image/files/a989f970b5994b8995bbd40edef29662.jpg"
-        },
-        {
-            "imgIdx" : "7",
-            "img" : "https://image.idus.com/image/files/1fcc98b600754f80aeec5e70a4ad43ef.jpg"
-        },
-    ]
+    const result = await Banner.getBanner();
+    console.log(result);
     
     res.status(statusCode.OK)
         .send(util.success(statusCode.OK, resMessage.BANNER_IMG_FIND_ALL, result));
